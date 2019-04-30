acaba6be3cc7ab87f4ee9ba549b0c3e6a7494aa4
JorgenBender <jorgenbender97@gmail.com>
2019-04-30 18:35:46 +0200

    Ikke revert denne

--- src/main/java/org/Forsikringsregister/IO/lesCsv.java
+++ src/main/java/org/Forsikringsregister/IO/lesCsv.java
@@ -1,4 +1,65 @@
 package org.Forsikringsregister.IO;
 
-public class lesCsv {
+import javafx.stage.FileChooser;
+import org.Forsikringsregister.Exceptions.InvalidKundeFormatException;
+import org.Forsikringsregister.Programlogikk.Kunde;
+
+import java.io.BufferedReader;
+import java.io.*;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+public class lesCsv implements IOInterface {
+    @Override
+    public ArrayList<Kunde> lesKundeliste() throws IOException, InvalidKundeFormatException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File(("data/")));
+        ArrayList<Kunde> importertListe = null;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(fileChooser.showOpenDialog(null)));
+            String line = null; // read first line
+
+            // read the rest and create Persons for each line
+            while((line=reader.readLine()) != null) {
+                importertListe.add(parsePerson(line));
+            }
+        } finally {
+            if(reader != null) {
+                reader.close();
+            }
+        }
+
+        return importertListe;
+    }
+
+    private static Kunde parsePerson(String line) throws InvalidKundeFormatException {
+        // split line string into three using the separator ";"
+        String[] split = line.split(";");
+        if(split.length != 3) {
+            throw new InvalidKundeFormatException("Must use semicolon ; to separate the three data fields");
+        }
+
+        String name = split[0];
+        int age = parseNumber(split[1], "Age (second field) is not a number");
+        int id = parseNumber(split[2], "Id (third field) is not a number");
+
+        return null; //new Kunde(name, age, id);
+    }
+
+    private static int parseNumber(String str, String errorMessage) throws InvalidKundeFormatException{
+        int number;
+        try {
+            number = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new InvalidKundeFormatException(errorMessage);
+        }
+
+        return number;
+    }
+
+    @Override
+    public void skrivKundeliste (ArrayList<Kunde> kundeliste) throws IOException {}
 }
